e03c22ca401b7cb2ba96f8c4fa91ca04a9d6ba1e
L <952603659@qq.com>
2020-08-09 19:09:09 +0800

    增加横向滑动，及个别ui

--- src/components/music-home-child/recommendMusic.vue
+++ src/components/music-home-child/recommendMusic.vue
@@ -1,66 +1,127 @@
 <template>
-    <div v-if="Object.keys(recommendMusic).length!==0" class="recommendMusic">
+    <div class="recommendMusic">
         <div class="topTitle">
             <h4>{{recommendMusic.uiElement.subTitle.title}}</h4>
             <btn-more :title="recommendMusic.uiElement.button.text"></btn-more>
         </div>
-        <div class="box4"
-             v-for="(item,index) in recommendMusic.creatives"
-             :key="index">
-            <van-cell-group :border="false">
-                <van-cell
-                        :center="true"
-                        :border="false"
-                        v-for="(value,indey) in item.resources"
-                        :key="indey"
-                        :title="value.uiElement.mainTitle.title"
-                        value="内容"
-                        :label="labelText(value)"
-                        label-class="ov"
-                        title-class="ov"
-                />
+        <horizontal-scroll ref="hScroll">
+            <div ref="list" class="list">
+                <div class="box4"
+                     v-for="(item,index) in recommendMusic.creatives"
+                     :key="index">
+                    <van-cell-group :border="false">
+                        <van-cell
+                                class="mc-cell"
+                                :center="true"
+                                :border="false"
+                                v-for="(value,indey) in item.resources"
+                                :key="indey"
 
-            </van-cell-group>
-        </div>
+                                :label="labelText(value)"
+                                :label-class="{'ov':true,'labelTextStyle':true,'labelColor':!isLabelColor}"
+                                title-class="ov titleText"
+                                @click="getMusicId(value.resourceId)">
+                            <!--                            :title="value.uiElement.mainTitle.title"-->
+                            <template #title>
+                                <p class="ov titleText">
+                                    {{value.uiElement.mainTitle.title}}
+                                    <span class="titleTextName">-&nbsp{{value.resourceExtInfo.artists[0].name}}</span>
+                                </p>
+                            </template>
+                            <template #icon>
+                                <van-image
+                                        class="leftImage"
+                                        width="50" height="50"
+                                        radius="5"
+                                        :src="value.uiElement.image.imageUrl" alt="">
+                                </van-image>
+                            </template>
+                            <template #right-icon>
+                                <van-icon name="play-circle-o"
+                                          size="24px"
+                                          color="#c2463a"
+                                />
+                            </template>
+                        </van-cell>
+
+                    </van-cell-group>
+                </div>
+            </div>
+
+        </horizontal-scroll>
+
     </div>
 </template>
 
 <script>
     // vant导入
-    import {Cell, CellGroup} from 'vant';
+    import {Cell, CellGroup, Image as VanImage, Icon} from 'vant';
+    import {initScroll} from "../common/mixin";
     import BtnMore from "../common/btnMore";
+    import HorizontalScroll from "../horizontalScroll";
 
     export default {
         name: "recommendMusic",
+        mixins: [initScroll],
         props: {
             recommendMusic: {
                 type: Object,
                 default: {}
             }
         },
+        mounted() {
+            this.$nextTick(() => {
+                this.itemWidth = 53;
+                this.itemMargin = 8;
+                setTimeout(() => {
+                    this.initPics();
+                    this.$refs.hScroll.refresh()
+
+                }, 600)
+            })
+        },
         computed: {
             labelText() {
                 return function (value) {
-                    console.log(value.uiElement.subTitle);
                     if (value.uiElement.subTitle !== undefined) {
-                        console.log(value.uiElement.subTitle.title);
+                        let str = value.uiElement.subTitle.title.substring(value.uiElement.subTitle.title.length - 3);
+                        this.isLabelColor = str !== '人播放';
                         return value.uiElement.subTitle.title
                     } else {
                         return ''
                     }
                 }
-
+            },
+        },
+        data() {
+            return {
+                isLabelColor: true
+            }
+        },
+        methods: {
+            getMusicId(musicId) {
+                // 音乐id
+                console.log(musicId);
             }
         },
         components: {
             [Cell.name]: Cell,
             [CellGroup.name]: CellGroup,
+            [VanImage.name]: VanImage,
+            [Icon.name]: Icon,
             BtnMore,
+            HorizontalScroll,
         }
     }
 </script>
 
 <style scoped lang="less">
+    .ovtext() {
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+    }
+
     .recommendMusic {
         margin: 50px 30px;
 
@@ -70,10 +131,49 @@
             margin-bottom: 30px;
             text-align: center;
         }
+
+        .list {
+            display: flex;
+            justify-content: space-between;
+            /*flex: 1;*/
+
+            .box4 {
+                width: 100vw;
+                margin-right: 50px;
+
+                .mc-cell {
+                    margin: 30px 15px 30px 0;
+                }
+
+                .leftImage {
+                    margin-right: 30px;
+                }
+
+                .titleText {
+                    width: 500px;
+                    font-weight: 600;
+                    font-size: 38px;
+
+                    .titleTextName {
+                        color: #a7a6a7;
+                        font-size: 26px;
+                    }
+                }
+
+                .labelColor {
+                    width: 210px;
+                    font-size: 24px;
+                    background: rgba(247, 230, 230, 1);
+                    color: @theme-color;
+                    text-align: center;
+                }
+            }
+        }
+
+        .ov {
+            .ovtext();
+
+        }
     }
-    .ov{
-        white-space: nowrap;
-        overflow: hidden;
-        text-overflow: ellipsis;
-    }
+
 </style>
