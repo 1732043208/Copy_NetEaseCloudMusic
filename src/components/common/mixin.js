44f5f400dddd9e26ccd381d2db9d14a00d39e672
L <952603659@qq.com>
2020-08-10 21:57:21 +0800

    增加格式化播放时长方法，控制播放状态

--- src/components/common/mixin.js
+++ src/components/common/mixin.js
@@ -1,4 +1,3 @@
-
 export const initScroll = {
     data() {
         return {
@@ -19,3 +18,44 @@ export const initScroll = {
         },
     }
 };
+
+// 将整数转换成 时：分：秒的格式
+export function realFormatSecond(second) {
+    let secondType = typeof second;
+    if (secondType === 'number' || secondType === 'string') {
+        second = parseInt(second);
+        let hours = Math.floor(second / 3600);
+        second = second - hours * 3600;
+        let mimute = Math.floor(second / 60);
+        second = second - mimute * 60;
+        return hours + ':' + ('0' + mimute).slice(-2) + ':' + ('0' + second).slice(-2)
+    } else {
+        return '00:00'
+    }
+}
+
+export const controlPlay = {
+    methods: {
+        // 控制音频的播放与暂停
+        startPlayOrPause(index) {
+            console.log(index);
+            this.isPlaying ? this.pause(index) : this.play(index)
+        },
+        // 播放音频
+        play(index) {
+            if (index === -1) return this.$refs.audio.play();
+            this.$refs.audio[index].play();
+        },
+        // 暂停音频
+        pause(index) {
+            if (index === -1) return this.$refs.audio.pause();
+            this.$refs.audio[index].pause();
+        },
+        onPlay() {
+            this.$store.state.isPlay = true;
+        },
+        onPause() {
+            this.$store.state.isPlay = false;
+        },
+    }
+}
