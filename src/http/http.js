687d4c6a724a08eb907c9636c5e8782a54780525
L <952603659@qq.com>
2020-08-10 22:01:43 +0800

    优化响应拦截器处理

--- src/http/http.js
+++ src/http/http.js
@@ -18,8 +18,20 @@ export function http(config) {
     instance.interceptors.response.use(result => {
         return result
     }, error => {
-        console.log('响应出错' + error);
-        Vue.prototype.$toast('网络出错');
+        console.log('响应出错');
+        if (error.response) {
+            if (error.response.status === 404) {
+                error.message = '请求出错404';
+                console.log(error.response);
+                if (error.response.data) Vue.prototype.$toast(error.response.data.message);
+                return error.message
+            }
+            if (error.response.status === 400) console.log('参数错误400');
+            if (error.response.status === 500) console.log('服务器错误500');
+        } else {
+            error.message = '连接服务器失败'
+        }
+        return error.message;
     });
     return instance(config);
 }
