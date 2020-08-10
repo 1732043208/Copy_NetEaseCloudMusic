66020eb4e0f0803b2204fcdaca70fe6bbac53557
L <952603659@qq.com>
2020-08-10 22:07:50 +0800

    挂载检查音乐是否可用方法

--- src/main.js
+++ src/main.js
@@ -2,13 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import base from './http/base'
 import './assets/common/common.less'
 
 // 全局引入vant组件
 import {Toast} from "vant";
+
 Vue.use(Toast);
 
+Vue.use(base);
 Vue.config.productionTip = false;
 
 new Vue({
