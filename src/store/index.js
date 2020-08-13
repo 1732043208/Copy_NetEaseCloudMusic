8bfa9a146225b908673192ccb3e0f85729812ed6
L <952603659@qq.com>
2020-08-13 09:27:37 +0800

    新增音乐url，是否展示抽屉，改变播放器的播放按钮

--- src/store/index.js
+++ src/store/index.js
@@ -6,10 +6,32 @@ Vue.use(Vuex)
 export default new Vuex.Store({
     state: {
         musicId: null,
-        isPlay:false,
+        musicUrl: null,
+        isPlay: false,
+        isShowDrawer: false,
+        changeIcon: false,
     },
     mutations: {
-
+        // musicId
+        changeMusicId(state, newId) {
+            state.musicId = newId;
+        },
+        changeMusicUrl(state, newUrl) {
+            state.musicUrl = newUrl;
+        },
+        // 正在播放
+        IsPlaying(state) {
+            state.isPlay = true;
+        },
+        NotPlaying(state) {
+            state.isPlay = false;
+        },
+        showIcon(state) {
+            state.changeIcon = !state.changeIcon;
+        },
+        toggleDrawer(state) {
+            state.isShowDrawer = !state.isShowDrawer
+        }
     },
     actions: {},
     modules: {}
