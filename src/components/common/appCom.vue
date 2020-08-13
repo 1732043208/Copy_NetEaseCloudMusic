b6d1f345842da89c643631745822b214772b6d8b
L <952603659@qq.com>
2020-08-13 09:16:44 +0800

    新增抽屉组件

---
+++ src/components/common/appCom.vue
@@ -0,0 +1,40 @@
+<template>
+    <drawer>
+        <div slot="drawer">
+
+            <transition
+                    enter-active-class="animate__fadeIn"
+                    leave-active-class="animate__fadeIn"
+                    :duration="{enter: 200, leave: 400}">
+
+                <h1>哈哈哈哈哈哈哈哈哈哈</h1>
+            </transition>
+
+        </div>
+        <div slot="content">
+            <home-nav v-if="$route.meta.isShow"></home-nav>
+            <keep-alive>
+                <router-view v-if="$route.meta.isShow"></router-view>
+            </keep-alive>
+            <router-view v-if="!$route.meta.isShow"></router-view>
+        </div>
+    </drawer>
+
+</template>
+
+<script>
+    import homeNav from '../nav/home-nav';
+    import drawer from "./drawer";
+
+    export default {
+        name: "appCom",
+        components: {
+            homeNav,
+            drawer
+        }
+    }
+</script>
+
+<style scoped lang="less">
+
+</style>
