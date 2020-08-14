8720290d51ff933713b2c01cd20f039462d6b813
L <952603659@qq.com>
2020-08-14 17:21:37 +0800

    增加抽屉遮罩层渐变动画和拖拽动画

--- src/components/common/appCom.vue
+++ src/components/common/appCom.vue
@@ -1,40 +1,277 @@
 <template>
-    <drawer>
-        <div slot="drawer">
+    <div class="appCom">
+        <drawer>
+            <div slot="drawer">
+                <scroll
+                        class="content"
+                        ref="scroll"
+                        :probe-type="3"
+                        :pull-up-load="true"
 
-            <transition
-                    enter-active-class="animate__fadeIn"
-                    leave-active-class="animate__fadeIn"
-                    :duration="{enter: 200, leave: 400}">
+                >
+                    <transition
+                            enter-active-class="animate__fadeIn"
+                            leave-active-class="animate__fadeIn"
+                            :duration="{enter: 200, leave: 400}">
 
-                <h1>哈哈哈哈哈哈哈哈哈哈</h1>
-            </transition>
-
-        </div>
-        <div slot="content">
-            <home-nav v-if="$route.meta.isShow"></home-nav>
-            <keep-alive>
-                <router-view v-if="$route.meta.isShow"></router-view>
-            </keep-alive>
-            <router-view v-if="!$route.meta.isShow"></router-view>
-        </div>
-    </drawer>
+                    </transition>
+                    <div class="content1">
+                        <div v-if="!isLogin" class="topContent">
+                            <p>还未登录，请登录</p>
+                            <van-button
+                                    to="/login"
+                                    @click="loginBtnClick"
+                                    class="loginBtn"
+                                    color="#c2463a"
+                                    round
+                                    type="info">立即登录
+                            </van-button>
+                        </div>
+                        <div v-if="isLogin" class="topContent">
+                            <van-cell :center="true">
+                                <template #icon>
+                                    <van-image
+                                            round
+                                            class="leftImage"
+                                            width="35"
+                                            height="35"
+                                            :src="userInfo.avatarUrl" alt="">
+                                    </van-image>
+                                </template>
+                                <template #title>
+                                    <span class="custom-title">{{userInfo.userName}}</span>
+                                </template>
+                                <template #right-icon>
+                                    <van-button color="#c2463a"
+                                                round
+                                                class="qdBtn"
+                                                size="mini "
+                                                type="primary">
+                                        签到
+                                    </van-button>
+                                </template>
+                            </van-cell>
+                        </div>
+                        <div class="mineFun">
+                            <van-grid>
+                                <van-grid-item @click="mineFunClick" icon="envelop-o" text="我的消息"/>
+                                <van-grid-item @click="mineFunClick" icon="friends-o" text="我的好友"/>
+                                <van-grid-item @click="mineFunClick" icon="wap-home-o" text="个人主页"/>
+                                <van-grid-item @click="mineFunClick" icon="shop-collect-o" text="个性装扮"/>
+                            </van-grid>
+                        </div>
+                        <div class="bottomContent">
+                            <van-cell-group>
+                                <van-cell
+                                        class="cellItem"
+                                        size="large"
+                                        :center="true"
+                                        v-for="(item,index) in cellItem1"
+                                        :icon="item.icon"
+                                        :value="item.title"
+                                        :key="index"
+                                        value-class="cellText"
+                                />
+                            </van-cell-group>
+                            <div style="margin: 30px 0">
+                                <van-divider/>
+                            </div>
+                            <van-cell-group>
+                                <van-cell
+                                        class="cellItem"
+                                        size="large"
+                                        :center="true"
+                                        v-for="(item,index) in cellItem2"
+                                        :icon="item.icon"
+                                        :value="item.title"
+                                        :key="index"
+                                        value-class="cellText"
+                                />
+                            </van-cell-group>
+                        </div>
+                    </div>
+                </scroll>
+            </div>
+            <div slot="content">
+                <home-nav v-if="$route.meta.isShow"></home-nav>
+                <keep-alive>
+                    <router-view v-if="$route.meta.isShow"></router-view>
+                </keep-alive>
+                <router-view v-if="!$route.meta.isShow"></router-view>
+            </div>
+        </drawer>
+    </div>
 
 </template>
 
 <script>
+    import scroll from "../scroll";
     import homeNav from '../nav/home-nav';
     import drawer from "./drawer";
+    import {Divider, Button, Grid, GridItem, Cell, CellGroup, Image as VanImage} from 'vant'
+    import {LoginStatusAPI} from "../../http/all-api";
+    import {userInfoModel} from "../../http/model";
 
     export default {
         name: "appCom",
+        created() {
+            LoginStatusAPI().then(res => {
+                console.log(res);
+                this.$store.state.userInfo = new userInfoModel(res.data.profile)
+                console.log(this.$store.state.userInfo);
+            }).catch(error => {
+                if (error.status === 301) {
+                    this.$toast('未登录')
+                }
+            })
+        },
+        computed: {
+            isLogin: {
+                get() {
+                    return this.$store.state.isLogin
+                },
+                set(nv) {
+                    return this.$store.state.isLogin = nv
+                }
+            },
+            userInfo: {
+                get() {
+                    return this.$store.state.userInfo
+                }
+            }
+        },
+        data() {
+            return {
+                cellItem1: [
+                    {
+                        icon: 'service-o',
+                        title: '听歌识曲'
+                    }, {
+                        icon: 'hot-sale',
+                        title: '演出'
+                    }, {
+                        icon: 'cart-o',
+                        title: '商城'
+                    }, {
+                        icon: 'location-o',
+                        title: '附近的人'
+                    }, {
+                        icon: 'volume-o',
+                        title: '口袋彩铃'
+                    }
+                ],
+                cellItem2: [
+                    {
+                        icon: 'balance-list-o',
+                        title: '我的订单'
+                    }, {
+                        icon: 'underway-o',
+                        title: '定时停止播放'
+                    }, {
+                        icon: 'scan',
+                        title: '扫一扫'
+                    }, {
+                        icon: 'bulb-o',
+                        title: '音乐闹钟'
+                    }
+                ]
+            }
+        },
+        methods: {
+            loginBtnClick() {
+                this.$store.commit('toggleDrawer');
+            },
+            mineFunClick() {
+                this.$store.commit('toggleDrawer');
+                if (this.$store.state.isLogin) {
+                    console.log('已经登录，跳转到xxx页面');
+                } else {
+                    this.$toast('请先登录');
+                    setTimeout(() => {
+                        this.$router.push({
+                            path: '/login'
+                        })
+                    }, 300)
+                }
+            }
+        },
         components: {
+            scroll,
             homeNav,
-            drawer
+            drawer,
+            [Divider.name]: Divider,
+            [Button.name]: Button,
+            [Grid.name]: Grid,
+            [GridItem.name]: GridItem,
+            [Cell.name]: Cell,
+            [CellGroup.name]: CellGroup,
+            [VanImage.name]: VanImage,
+
         }
     }
 </script>
 
 <style scoped lang="less">
+    .ovtext() {
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+    }
 
+    .appCom {
+        width: 100vw;
+        height: 100vh;
+
+        .content {
+            overflow: hidden;
+            position: absolute;
+            top: 0;
+            bottom: 0;
+            left: 0;
+            right: 0;
+            z-index: 0;
+            touch-action: none;
+        }
+
+        .content1 {
+            padding-top: 50px;
+            text-align: center;
+        }
+
+        .topContent {
+            margin-left: 50px;
+            font-size: 34px;
+            color: rgb(75, 75, 75);
+
+            .loginBtn {
+                margin-top: 30px;
+                padding: 0 100px;
+            }
+
+            .qdBtn {
+                font-size: 30px;
+                margin-right: 30px;
+                padding: 0 30px;
+            }
+
+            .custom-title {
+                font-weight: bold;
+            }
+        }
+
+        .mineFun {
+            margin-top: 30px;
+        }
+
+        .bottomContent {
+            .cellItem {
+                padding: 50px 30px;
+            }
+
+            .cellText {
+                padding-left: 20px;
+                font-size: 40px;
+            }
+        }
+    }
 </style>
--- src/components/common/drawer.vue
+++ src/components/common/drawer.vue
@@ -1,8 +1,15 @@
 <template>
     <div
-            class="drawer-layout">
+            class="drawer-layout"
+            @touchstart="handleTouchStart"
+            @touchmove="handleTouchMove"
+            @touchend="handleTouchEnd"
+            @touchmove.prevent
+
+    >
         <transition name="move1">
             <div
+                    ref="drawer_wrap"
                     v-show="isShowDrawer"
                     class="drawer-wrap">
                 <!--            抽屉里的内容-->
@@ -23,10 +30,13 @@
             <slot name="content"></slot>
         </div>
 
+
     </div>
 </template>
 
 <script>
+    import HorizontalScroll from "../horizontalScroll";
+
     export default {
         name: "drawer",
         computed: {
@@ -39,7 +49,72 @@
         methods: {
             clockDrawer() {
                 this.$store.commit('toggleDrawer');
+            },
+            // 当手指触摸屏幕时候触发
+            handleTouchStart(touch) {
+                // 记录开始触摸位置的X轴
+                this.startPos = touch.touches[0].pageX;
+            },
+            // 当手指在屏幕上滑动的时候连续地触发
+            handleTouchMove(touch,e) {
+                // e.preventDefault(); // 可忽略
+                // console.log(touch);
+                let touchMove = touch.changedTouches[0].pageX; // 记录移动的位置
+                // 移动位置 - 刚开始触摸的位置
+                let moveResult = touchMove - this.startPos;
+
+                // 刚开始 moveX = 0
+                if (this.moveX > moveResult) { // 判断移动
+                    // 当向左滑时，moveResult为负数，判断成立
+                    // 保存这次 相减的结果
+                    // 举例： 当我先是往左滑，再往右滑时，
+                    // 往左滑保存的位置 -20
+                    // 突然想往右滑
+                    // 这时 moveResult的值就会比 往左滑保存的位置 大！！
+                    // 至于为什么，自己动脑想想
+                    this.moveX = moveResult;
+
+                    // 因为我css的left选择用 % 作为单位 所有这里要不能超过100
+                    // 左滑隐藏 所有需要left = -100%才能隐藏
+                    if (this.leftMove > -290) {
+                        this.leftMove -= 6;
+                        this.$refs.drawer_wrap.style.left = this.leftMove + 'px';
+                    }
+
+                } else {
+                    // drawer—wrap这个类用了绝对定位，所以left要为0，左边才不会有间隙
+                    if (this.leftMove < 0) {
+                        this.leftMove += 6;
+                        this.$refs.drawer_wrap.style.left = this.leftMove + 'px';
+                    } else if (this.leftMove === 0) {
+                        // 当leftMove为0时，也就是抽屉完全展开的状态
+                        // 所以要重置moveX的值，也就是让最上面的if重新开始判断
+                        this.moveX = 0
+                    }
+                }
+            },
+            handleTouchEnd(touch) {
+                if (this.leftMove <= -100) {
+                    this.$store.commit('toggleDrawer');
+                    this.$store.state.isShowDrawer = false;
+                } else {
+                    this.$refs.drawer_wrap.style.left = 0 + 'px';
+                }
+                // 触摸结束后，初始化全部记录的位置
+                this.moveX = 0;
+                this.startPos = 0;
+                this.leftMove = 0;
+            },
+        },
+        data() {
+            return {
+                startPos: 0, // 记录开始触摸的X轴位置
+                leftMove: 0, // 左边移动了多少
+                moveX: 0 // X轴移动
             }
+        },
+        components: {
+            HorizontalScroll
         }
     }
 </script>
@@ -48,16 +123,20 @@
     .drawer-layout {
         width: 100vw;
         height: 100vh;
+        overflow-x: hidden;
 
 
         .drawer-wrap {
             width: 70%;
             height: 100vh;
             position: absolute;
+            left: 0;
             z-index: 300;
-            color: pink;
             background-color: #fff;
-
+            word-break: break-all;
+            /*overflow-y: auto;*/
+            /*overflow-x: hidden;*/
+            /*-webkit-overflow-scrolling: touch;*/
         }
 
         .drawer-mask {
@@ -72,7 +151,7 @@
 
     /* 动画开始 过度效果 enter是 隐藏->显示 leave是 显示->隐藏 */
     .fade-enter-active, .fade-leave-active {
-        transition: opacity 1s;
+        transition: opacity 0.5s;
     }
 
     /* 显示前或隐藏后的效果 */
@@ -81,7 +160,7 @@
     }
 
     .move1-enter-active, .move1-leave-active {
-        transition: all 1s;
+        transition: all 0.5s;
     }
 
     /* 显示前或隐藏后的效果 */
