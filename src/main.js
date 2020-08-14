494e68718295fadcda25164838ab85ae3c8efc20
L <952603659@qq.com>
2020-08-14 17:35:12 +0800

    加载即时获取登录状态

--- src/main.js
+++ src/main.js
@@ -5,14 +5,15 @@ import store from './store'
 import base from './http/base'
 import './assets/common/common.less'
 
+
 // 全局引入vant组件
 import {Toast} from "vant";
 
 Vue.use(Toast);
-
 Vue.use(base);
 Vue.config.productionTip = false;
 
+store.state.isLogin = sessionStorage.token !== undefined;
 new Vue({
     router,
     store,
