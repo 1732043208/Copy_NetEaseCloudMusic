1e5bfe9459e52a6fc547225f9742291e9979c456
黄伟杰 <1732043208@qq.com>
2020-09-11 11:16:49 +0800

    commit 新增搜索(视频)页面

--- src/views/search/searchResultChild/searchTabbar.vue
+++ src/views/search/searchResultChild/searchTabbar.vue
@@ -1,12 +1,18 @@
 <template>
     <div>
-        <van-tabs :swipeable="true" >
+        <van-tabs :swipeable="true">
             <van-tab v-for="(item,index) in tabbarList" :key="index" :title="item">
-               <div v-if="index === 0">
-                   <SearchTabbarZongHe @backtop="getbacktop"></SearchTabbarZongHe>
-               </div>
+                <div v-if="index === 0">
+                    <SearchTabbarZongHe @backtop="getbacktop"></SearchTabbarZongHe>
+                </div>
                 <div v-if="index ===1">
                     <SearchTabbarDanQu></SearchTabbarDanQu>
+                </div>
+                <div v-if="index===3">
+                    <SearchTabbarShiPin></SearchTabbarShiPin>
+                </div>
+                <div v-if="index===4">
+                 <SearchTabbarGeShou></SearchTabbarGeShou>
                 </div>
             </van-tab>
         </van-tabs>
@@ -16,26 +22,32 @@
 <script>
     import SearchTabbarZongHe from "./searchTabbarChild/SearchTabbarZongHe";
     import SearchTabbarDanQu from "./searchTabbarChild/SearchTabbarDanQu";
-    import { Cell, CellGroup } from 'vant';
-    import { Tab, Tabs } from 'vant';
+    import SearchTabbarShiPin from "./searchTabbarChild/SearchTabbarShiPin";
+    import SearchTabbarGeShou from "./searchTabbarChild/SearchTabbarGeShou";
+    import {Cell, CellGroup} from 'vant';
+    import {Tab, Tabs} from 'vant';
+
     export default {
         name: "searchTabbar",
-        data(){
-            return{
-                tabbarList:['综合','单曲','云村','视频','歌手','专辑','歌单','主播电台','用户'],
+        data() {
+            return {
+                tabbarList: ['综合', '单曲', '云村', '视频', '歌手', '专辑', '歌单', '主播电台', '用户'],
 
             }
         },
-        components:{
+        components: {
             SearchTabbarZongHe,
             SearchTabbarDanQu,
-            [Cell.name]:Cell,
-            [CellGroup.name]:CellGroup,
-            [Tab.name]:Tab,
-            [Tabs.name]:Tabs
+            SearchTabbarShiPin,
+            SearchTabbarGeShou,
+            [Cell.name]: Cell,
+            [CellGroup.name]: CellGroup,
+            [Tab.name]: Tab,
+            [Tabs.name]: Tabs,
+
         },
-        methods:{
-            getbacktop(){
+        methods: {
+            getbacktop() {
                 this.$emit('backtop')
             }
         }
---
+++ src/views/search/searchResultChild/searchTabbarChild/SearchTabbarShiPin.vue
@@ -0,0 +1,79 @@
+<template>
+    <div>
+        <div v-for="item in this.videos">
+            <van-card @click="getVideoDetailData(item.vid)">
+                <template #thumb>
+                    <img :src="item.coverUrl" style="width: 130px;height: auto">
+                    <span style="position: absolute;left: 85px;color: white">{{Math.round(item.playTime/10000*Math.pow(10,1))/Math.pow(10,1)}}万</span>
+                </template>
+                <template #title>
+                    <div style="font-size: 14px;margin-left: 46px;white-space: pre-wrap;margin-top: 4px;margin-bottom: 6px">
+                        {{item.title}}
+                    </div>
+                </template>
+                <template #tags>
+                    <span style="margin-left: 46px;">{{item.durationms|formatDuring}}</span>
+                    <span> by </span>
+                    <span v-for="creator in item.creator">{{creator.userName}}</span>
+                </template>
+            </van-card>
+        </div>
+    </div>
+</template>
+
+<script>
+    import {Icon} from "vant";
+    import {Button} from 'vant';
+    import {Card} from 'vant';
+    import {Tag} from 'vant';
+    import {formatDuring} from "../../../../components/common/utils";
+    import {Cell, CellGroup} from 'vant';
+    import {GetSearchApi} from "../../../../http/all-api";
+
+    export default {
+        name: "SearchTabbarShiPin",
+        data(){
+          return{
+              videos:[]
+          }
+        },
+        components:{
+            [Icon.name]: Icon,
+            [Button.name]: Button,
+            [Card.name]: Card,
+            [Tag.name]: Tag,
+            [Cell.name]: Cell,
+            [CellGroup.name]: CellGroup,
+        },
+        created() {
+            GetSearchApi(this.$store.state.addWord,'1014').then(res => {
+                this.videos = res.data.result.videos;
+                console.log(res);
+
+            }).catch(error => {
+                console.log(error);
+            });
+        },
+        filters: {
+            // 将整数转化成分秒
+            formatDuring(second = 0) {
+                return formatDuring(second)
+            },
+
+        },
+        methods:{
+            getVideoDetailData(vid) {
+                this.$router.push({
+                    path: '/videoDetail',
+                    query: {
+                        vid: vid
+                    }
+                })
+            },
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
